ea2e299b7cfd77062c4e8c3156107f7b67c795d8
yukisato <yuki01377@gmail.com>
2024-01-01 23:10:25 +0900

    デフォルトレイアウトを修正

--- front/zatudan-front/src/pages/_app.tsx
+++ front/zatudan-front/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app'
 import '@/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <div className={`${inter.className} container mx-auto py-8`}>
+            <Component {...pageProps} />
+        </div>
+    )
 }
---
+++ front/zatudan-front/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-gray-100">
+        <header className="bg-gray-800 text-white text-center py-4">
+          <h1 className="text-3xl font-bold">雑談のためのニュース</h1>
+        </header>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
