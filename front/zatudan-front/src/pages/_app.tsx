7d7ed2112f82fe5c44d53ee33a10f60c79c081d9
yukisato <yuki01377@gmail.com>
2024-01-03 14:29:06 +0900

    ga対応

--- front/zatudan-front/src/pages/_app.tsx
+++ front/zatudan-front/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { AppProps } from 'next/app'
 import '@/globals.css'
 import { Inter } from 'next/font/google'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <div className={`${inter.className} container mx-auto py-8`}>
-            <Component {...pageProps} />
-        </div>
+        <>
+            <GoogleAnalytics />
+            <div className={`${inter.className} container mx-auto py-8`}>
+                <Component {...pageProps} />
+            </div>
+        </>
     )
 }
