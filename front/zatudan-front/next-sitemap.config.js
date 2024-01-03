a1b7efc475cabb154fa8ce5fa48025a1b993d99d
yukisato <yuki01377@gmail.com>
2024-01-03 17:37:38 +0900

    サイトマップを追加

---
+++ front/zatudan-front/next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'news.zatudan.click', // カスタムドメイン
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
+    outDir: './out', // 出力先ディレクトリ
+};
