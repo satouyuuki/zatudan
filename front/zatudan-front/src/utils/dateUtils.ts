072b8b907e5e6eb3d3973fd729f14980cc70d897
yukisato <yuki01377@gmail.com>
2024-01-03 15:09:30 +0900

    jst時間を正しくする

--- front/zatudan-front/src/utils/dateUtils.ts
+++ front/zatudan-front/src/utils/dateUtils.ts
@@ -1,5 +1,5 @@
 const UTCtoJST = (utcDateString: string) => {
-    const utcDate = new Date(utcDateString + " UTC");
+    const utcDate = new Date(utcDateString);
     utcDate.setHours(utcDate.getHours() + 9);
     return utcDate.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
 };
