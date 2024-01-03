2c1946017156b31f724c772aee527ff06bc54dc4
yukisato <yuki01377@gmail.com>
2024-01-03 19:18:59 +0900

    ドメインを変更

--- front/zatudan-front/next-sitemap.config.js
+++ front/zatudan-front/next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'news.zatudan.click', // カスタムドメイン
+    siteUrl: 'zatudan.click', // カスタムドメイン
     generateRobotsTxt: true,
     sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
     outDir: './out', // 出力先ディレクトリ
