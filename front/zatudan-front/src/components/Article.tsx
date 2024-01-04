db87ee3a4bbceb6d4c3021df94b4520edc3242a2
yukisato <yuki01377@gmail.com>
2024-01-04 09:18:41 +0900

    worningの修正

--- front/zatudan-front/src/components/Article.tsx
+++ front/zatudan-front/src/components/Article.tsx
@@ -6,7 +6,12 @@ const Article = ({ article }: { article: ArticleProps }) => {
     return <div className="border border-gray-300 rounded-md shadow-md p-4">
         <h2 className="text-2xl font-bold mb-2">{article.title}</h2>
         <p className="text-gray-600 mb-4">{article.description}</p>
-        <p className="text-sm text-blue-500 hover:underline"><a href={article.link} target='_blank'>もっと見る</a></p>
+        <p className="text-sm text-blue-500 hover:underline">
+            <a
+                href={article.link}
+                target='_blank'
+                rel="noopener noreferrer">もっと見る</a>
+        </p>
         <p className="text-gray-400 text-xs">公開日: {jstPubDate}</p>
         <div className="mt-2">
             <div className="flex flex-wrap gap-2">
--- front/zatudan-front/src/pages/_app.tsx
+++ front/zatudan-front/src/pages/_app.tsx
@@ -1,13 +1,33 @@
 import { AppProps } from 'next/app'
 import '@/globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <div className={`${inter.className} container mx-auto py-8`}>
-            <Component {...pageProps} />
-        </div>
+        <>
+            <Head>
+                <title>雑談ニュース | 新鮮な話題を毎日更新！</title>
+                <meta name="description" content="雑談のネタになるニュースを定期的にお届け。ビジネス、スポーツ、テクノロジー、トレンドなど幅広い話題をカバーします。"></meta>
+                <meta name="author" content="yukisato"></meta>
+                <link rel="icon" href="/favicon/favicon.ico" />
+                <meta property="og:title" content="雑談ニュース | 新鮮な話題を毎日更新！" />
+                <meta property="og:description" content="雑談のネタになるニュースを定期的にお届け。ビジネス、スポーツ、テクノロジー、トレンドなど幅広い話題をカバーします。" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://zatudan.click" />
+                <meta property="og:image" content="/images/zatudanapp.png" />
+                <meta property="og:site_name" content="雑談ニュース" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="雑談ニュース | 新鮮な話題を毎日更新！" />
+                <meta name="twitter:description" content="雑談のネタになるニュースを定期的にお届け。ビジネス、スポーツ、テクノロジー、トレンドなど幅広い話題をカバーします。" />
+                <meta name="twitter:image" content="/images/zatudanapp.png"></meta>
+                <meta name="google-adsense-account" content="ca-pub-5709217848867200"></meta>
+            </Head>
+            <div className={`${inter.className} container mx-auto py-8`}>
+                <Component {...pageProps} />
+            </div>
+        </>
     )
 }
